9884ef2009bf2fcf081ce1d33a3084156a3ba111
carlton anand <carlton.anand@gmail.com>
2020-02-12 22:42:59 +0000

    adding number of fixtures played

--- app.js
+++ app.js
@@ -32,16 +32,20 @@ const getPlayerHistory = (id) => {
     const data1 = data.history
     //filtering through player gameweeks to retreive points where minutes > 1
     var total = 0
+    var fixturesPlayed = 0
     for (let gameweek = 0; gameweek < 25; gameweek++) {
       if (data1[gameweek].minutes > 1) {
         total = total + data1[gameweek].total_points
+        fixturesPlayed = fixturesPlayed + 1
       }
     }
     // console.log(data1)
     
-    return total
+    return [total, fixturesPlayed]
   })
 }
 
-console.log (getPlayerHistory(215))
+console.log (getPlayerHistory(532))
+
+///532 players?///
 
