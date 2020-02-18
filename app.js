1426a229394f8d343f09e48069039870161acb63
carlton anand <carlton.anand@gmail.com>
2020-02-18 17:05:57 +0000

    method to search id by player name

--- app.js
+++ app.js
@@ -56,14 +56,28 @@ const getFixtureDifficulty = (id, gameweek) => {
   })
 }
 
-console.log (getPlayerHistory(191, 0))
+// console.log (getFixtureDifficulty(191, 5))
 
 const getBasic = () => {
   const data = doCORSRequest(`${reqType.bootstrap}`);
   return data;
 }
-// console.log(getBasic())
+console.log(getBasic())
 
+// const getPlayerID = (firstName, lastName) => {
+//   const data = doCORSRequest(`${reqType.bootstrap}`);
+//   return data.then(data =>{ 
+//     const data1 = data.elements
+//     var id = 0
+//     for (let element = 0; element < 623; element++) {
+//       if (data1[element].first_name == firstName && data1[element].last_name == lastName) {
+//         id = id + data1[element].id
+//       }
+//     }
+//   return id;
+//   })
+// }
+// console.log(getPlayerID('sadio', 'mane'))
 
 const getMostCaptained = (gameweek) => {
   const data = doCORSRequest(`${reqType.bootstrap}`);
