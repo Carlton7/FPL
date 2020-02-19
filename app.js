452cee01a70a5c7942fdb184950d55c86803b9d8
carlton anand <carlton.anand@gmail.com>
2020-02-19 23:50:07 +0000

    changing gameweek param to only look at next gameweek available

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const getFixtureDifficulty = (id, gameweek) => {
   })
 }
 
-// console.log (getFixtureDifficulty(191, 5))
+console.log (getFixtureDifficulty(191, 0))
 
 const getBasic = () => {
   const data = doCORSRequest(`${reqType.bootstrap}`);
@@ -64,20 +64,20 @@ const getBasic = () => {
 }
 console.log(getBasic())
 
-// const getPlayerID = (firstName, lastName) => {
-//   const data = doCORSRequest(`${reqType.bootstrap}`);
-//   return data.then(data =>{ 
-//     const data1 = data.elements
-//     var id = 0
-//     for (let element = 0; element < 623; element++) {
-//       if (data1[element].first_name == firstName && data1[element].last_name == lastName) {
-//         id = id + data1[element].id
-//       }
-//     }
-//   return id;
-//   })
-// }
-// console.log(getPlayerID('sadio', 'mane'))
+const getPlayerID = (firstName, lastName) => {
+  const data = doCORSRequest(`${reqType.bootstrap}`);
+  return data.then(data =>{ 
+    const data1 = data.elements
+    var id = 0
+    for (let element = 0; element < 623; element++) {
+      if (data1[element].first_name == firstName && data1[element].second_name == lastName) {
+        id = id + data1[element].id
+      }
+    }
+  return id;
+  })
+}
+console.log(getPlayerID('Mohamed', 'Salah'))
 
 const getMostCaptained = (gameweek) => {
   const data = doCORSRequest(`${reqType.bootstrap}`);
